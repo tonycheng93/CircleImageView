a7b41c4d7508e19f3722ccb3881ce80014431c09
tonycheng <bc1341286335@gmail.com>
2015-09-20 19:18:03 +0800

    Initial commit

---
+++ app/src/main/java/com/tony/circleimageview/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tony.circleimageview.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tony.circleimageview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
